193431d518c6d37eaefa0e0ef72c65d68fa57a3c
sa02045 <sa02045@naver.com>
2023-02-19 19:28:32 +0900

    init commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>FE Developer</h1>
+      <span>개발자 조승희</span>
+      <nav>
+        <ul>
+          <li>Posts</li>
+          <li>Resume</li>
+          <li>Github</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
