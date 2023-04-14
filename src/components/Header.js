7daf3ed6b016600a02de1279b39ac038ee783a19
sa02045 <sa02045@naver.com>
2023-04-14 21:30:47 +0900

    feat: 클릭할 수 있는 링크 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
+import React, { useEffect, useState } from "react"
 import quotes from "../constants/quotes"
 
 function Header({ location }) {
-  const rootPath = `${__PATH_PREFIX__}/`
   const [randomQuote, setRandomQuote] = useState({})
 
   useEffect(() => {
@@ -11,13 +10,12 @@ function Header({ location }) {
     setRandomQuote(quotes[randomNumber])
   })
 
-  console.log(location)
-  // const isRootPath = location.pathname === rootPath
-
   return (
     <header className="header">
       <div className="header__title-container">
-        <h1>소프트엔지니어 블로그</h1>
+        <Link to="/">
+          <h1>소프트엔지니어 블로그</h1>
+        </Link>
         <div className="header__sub-title">
           <q>{randomQuote.quote}</q>
           <div> - {randomQuote.author}</div>
@@ -26,16 +24,12 @@ function Header({ location }) {
       <nav>
         <ul>
           <Link to="/">
-            <li className={location.pathname === "/" ? "clicked" : ""}>
-              Posts
-            </li>
+            <li className={location.pathname === "/" ? "clicked" : ""}>Posts</li>
           </Link>
           <Link to="/resume/">
-            <li className={location.pathname === "/resume/" ? "clicked" : ""}>
-              Resume
-            </li>
+            <li className={location.pathname === "/resume/" ? "clicked" : ""}>Resume</li>
           </Link>
-          <Link to="https://github.com/sa02045">
+          <Link to="https://github.com/sa02045/">
             <li>Github</li>
           </Link>
         </ul>
