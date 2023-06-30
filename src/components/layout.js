b4c2e7d8318eef67714e2a8040dfa97efada5ea8
joSeugHee <seunghee.jo@8percent.kr>
2023-07-01 01:32:09 +0900

    style 고래 css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,24 @@
-import * as React from "react"
-import Header from "./Header"
+import * as React from "react";
+import Header from "./Header";
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
+  const rootPath = `${__PATH_PREFIX__}/`;
+  const isRootPath = location.pathname === rootPath;
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <Header location={location} />
       <main>{children}</main>
       <footer></footer>
+      <div className="whaleContainer">
+        <div className="whalePos size1">
+          <div className="whaleRotate size1">
+            <div className="whale"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
