5bf615e7a9b11abdd1f9de9cf2e3c327e1c82140
joseunghee <sa02045@naver.com>
2023-12-26 21:20:33 +0900

    fix: change gatsby-plugin-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,7 +130,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
         head: true,
