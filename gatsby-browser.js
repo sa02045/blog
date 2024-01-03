698a2432f8cafe9e6d94d93614a0f03e2c6d54bb
joseunghee <sa02045@naver.com>
2024-01-03 23:13:01 +0900

    chore: clean css code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,2 @@
-// custom typefaces
-import "@fontsource/montserrat/variable.css"
-import "@fontsource/merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
+import "./src/css/normalize.css";
+import "./src/css/style.css";
