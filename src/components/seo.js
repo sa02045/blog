9ee4c6a97a8e172d44ba0a70e0b0febc66a94aa3
joseunghee <sa02045@naver.com>
2024-01-09 00:02:57 +0900

    add seo meta tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,10 +26,15 @@ const Seo = ({ description, title, children, thumbnailURL }) => {
       <html lang="ko" />
       <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
       <meta name="description" content={metaDescription} />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="og:image" content={thumbnailURL} />
+      <meta property="og:locale" content="ko_KR" />
+      <meta property="og:image" content={thumbnailURL} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata?.social?.twitter || ``} />
       <meta name="twitter:image" content={thumbnailURL} />
