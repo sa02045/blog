815ad9528775078b48a6caf308a0617e0f88bf38
joseunghee <sa02045@naver.com>
2024-01-10 00:01:06 +0900

    fix: twitter image

--- src/components/seo.js
+++ src/components/seo.js
@@ -22,6 +22,7 @@ const Seo = ({ description, title, children, thumbnailURL }) => {
   const metaDescription = description || site.siteMetadata.description;
   const defaultTitle = site.siteMetadata?.title;
   const siteUrl = site.siteMetadata?.siteUrl;
+  const thumbnailURL = thumbnailURL || '/images/og-image.png';
 
   return (
     <>
@@ -33,13 +34,12 @@ const Seo = ({ description, title, children, thumbnailURL }) => {
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="ko_KR" />
+
       <meta property="og:image" content={siteUrl + thumbnailURL} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
+      <meta name="twitter:image" content={siteUrl + thumbnailURL} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata?.social?.twitter || ``} />
-      <meta name="twitter:image" content={site.siteUrl + thumbnailURL} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       {children}
