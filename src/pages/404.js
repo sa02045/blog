9c0153c69009160ff1a2d70a115bdd41e06ad0e0
joseunghee <sa02045@naver.com>
2024-01-13 00:05:17 +0900

    feat: 404 page redirect home

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,11 @@
-import * as React from 'react';
-import { graphql } from 'gatsby';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
+const NotFoundPage = () => {
+  useEffect(() => {
+    navigate('/');
+  }, []);
 
-import Layout from '../components/layout';
-import Seo from '../components/seo';
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title;
-
-  return <Layout location={location} title={siteTitle}></Layout>;
+  return <div></div>;
 };
 
-export const Head = () => <Seo title="404: Not Found" />;
-
 export default NotFoundPage;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
