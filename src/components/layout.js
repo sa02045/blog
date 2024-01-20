cc96b0d2fdfda184fad3aac307e03185349943d7
joseunghee <sa02045@naver.com>
2024-01-20 22:42:09 +0900

    feat: add footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Header from './Header';
+import { Footer } from './Footer';
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 deckDeckGoHighlightElement();
 
@@ -11,7 +12,7 @@ const Layout = ({ location, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <Header location={location} />
       <main>{children}</main>
-      <footer></footer>
+      <Footer></Footer>
     </div>
   );
 };
