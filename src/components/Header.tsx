8fbcd1fb549afafe59926cc41f0d5c7a95021e0e
devjo <devjo@devjoui-MacBookPro.local>
2024-04-28 16:15:53 +0900

    feat: header max width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Header() {
   return (
     <header className="px-6 py-4 border-b">
-      <div className="flex justify-between max-w-screen-lg mx-auto">
+      <div className="flex justify-between max-w-screen-md mx-auto">
         <ul className="flex">
           <Link to="/">
             <li className="text-2xl text-violet-500 font-bold">seunghee 기술블로그</li>
